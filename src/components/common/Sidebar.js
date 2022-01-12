1318c3e0c555011f5ccc6b95f37fde71bef38191
Danji-ya <erangerang15@gmail.com>
2022-01-12 18:03:53 +0900

    Remove: 불필요한 파일 및 폴더 삭제

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ToggleModeContainer from '../../containers/ToggleModeContainer';
-import AccountContainer from '../../containers/AccountContainer';
 import { SidebarContainer } from '../../styles/sidebarStyle';
 
 function Sidebar() {
   return (
     <SidebarContainer>
       <ToggleModeContainer />
-      {/* <AccountContainer /> */}
     </SidebarContainer>
   );
 }
