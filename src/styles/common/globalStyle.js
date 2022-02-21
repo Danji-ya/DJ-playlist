18e78b110afdf831c320b783686f4c1c75790500
Danji-ya <erangerang15@gmail.com>
2022-02-21 11:47:26 +0900

    [style]: 불필요한 스크롤 스타일 삭제

--- src/styles/common/globalStyle.js
+++ src/styles/common/globalStyle.js
@@ -9,15 +9,6 @@ const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.converseColor};
     background: ${({ theme }) => theme.body};
     transition: all 0.5s linear;
-    &::-webkit-scrollbar {
-    width: 15px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #5E5E5E;
-  }
-  &::-webkit-scrollbar-track {
-    background-color: ${({ theme }) => theme.bodySecondary};
-  }
   }
 `;
 
