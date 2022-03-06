39e96b0d33563bc294f1200c0340a34961d8ef6d
Danji-ya <erangerang15@gmail.com>
2022-03-07 00:24:05 +0900

    [refactor]: font preload 적용

--- src/styles/common/global.ts
+++ src/styles/common/global.ts
@@ -5,6 +5,13 @@ import reset from './reset';
 const GlobalStyles = createGlobalStyle`
   ${reset}
 
+  @font-face {
+    font-family: 'BMHANNAPro';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_seven@1.0/BMHANNAPro.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   * {
     box-sizing: border-box;
   }
